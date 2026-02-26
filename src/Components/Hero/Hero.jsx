dc21d326bd255ef3b0a07d4ad17507ca139a9bde
shova <shovasapkota101@gmail.com>
2026-02-26 21:34:54 +0545

    I have create textchange file for making animated text

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Shova_Img from '../../assets/shova.jpg'
+import TextChange from '../TextChange'
 
 const Hero = () => {
   return (
     <div className='text-(--primary-color) flex w-full items-start p-10 md:p-20 lg:p-25'>
       <div className='w-full  md:pt-10 text-left'>
-      <h1 className='text-2xl md:text-3xl lg:text-5xl font-bold flex leading-normal tracking-tighter'>Hello! I am Shova</h1>
+      <h1 className='text-2xl md:text-3xl lg:text-5xl font-bold flex leading-normal tracking-tighter'><TextChange/></h1>
       <p className='text-sm md:text-xl lg:text-2xl tracking-tight'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nostrum, harum, dignissimos quas.</p>
       <button className='mt-5 md:mt-7 lg:mt-10 py-2 px-2 md:px-3 lg:px-4 text-sm md:text-lg lg:text-xl hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]'>Contact Me</button>
       </div>
